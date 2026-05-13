d6abfada2713488fa43b57e91317c5116582c222
raghanipiyush <raghanipiyush1@gmail.com>
2026-05-13 17:42:13 +0530

    Add files via upload

---
+++ PR2.1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int score;
+    char grade;
+
+    printf("Enter your score: ");
+    scanf("%d", &score);
+
+    grade = (score >= 90) ? 'A' :
+            (score >= 80) ? 'B' :
+            (score >= 70) ? 'C' :
+            (score >= 60) ? 'D' :
+            (score >= 50) ? 'E' : 
+            'F';
+
+    printf(" your Grade is = %c",grade);
+}
+/*output
+Enter your score: 92
+ your Grade is = A
+*/
+    
